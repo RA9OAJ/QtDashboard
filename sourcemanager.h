b5bebee93ad6a1f2639dbb7cd27445839ba91103
RA9OAJ <ra9oaj@gmail.com>
2015-07-17 14:40:48 +0600

    WidgetManager

--- sourcemanager.h
+++ sourcemanager.h
@@ -2,16 +2,37 @@
 #define SORCEMANAGER_H
 
 #include <QObject>
+#include <QString>
 
-class SorceManager : public QObject
+class SourceManager : public QObject
 {
     Q_OBJECT
 public:
-    explicit SorceManager(QObject *parent = 0);
+    explicit SourceManager(QObject *parent = 0);
+
+    Q_PROPERTY(QString source READ source NOTIFY sourceChanget)
+
+    QString source() const;
 
 signals:
+    void sourceChanget();
+    void nextSourceChanget();
+    void volumeChanget();
+    void soppedSource();
 
 public slots:
+    /*void goNext();
+    void goPrev();
+    void nextLoaded();
+    void nextError();*/
+
+private:
+    int _cur_id;
+    bool _audio;
+    QString _source;
+    QString _type;
+    int _visibilityInterval;
+    qreal _volume;
 
 };
 
